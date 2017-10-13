aa2d921ce437bbc2a64c92d6d239096d1d4d0f40
Rob Chang <robchangcanada@gmail.com>
2017-10-13 13:54:50 -0400

    first upload

---
+++ dev/scripts/app.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { 
+    BrowserRouter as Router, 
+    Route, Link, NavLink } from 'react-router-dom';
+class App extends React.Component{
+	constructor(props){
+		super();
+		this.state={
+		};
+	}
+	render(){
+		return(
+			<div> 
+				
+			</div> 
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
